18e5c48e0f0916d462a7d0272449a185f1173472
brucopi <brucopi12@gmail.com>
2023-02-12 15:10:45 -0300

    arrumou a msg

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Ola, direto de Sorocaba");
+    expect(hello.hello()).toEqual("Ola, direto de Sorocaba!");
   });
 });
